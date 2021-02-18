7ef36954d1c38b393062778e64999156be1ac5be
offlineProgrammer <mmalaka@gmail.com>
2021-02-17 21:59:51 -0800

    TimerQuestionScoreRow Widget

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,3 +1,4 @@
+import 'package:brain_trainer_app/widgets/timer_question_score.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatelessWidget {
@@ -9,88 +10,7 @@ class GameScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-              //crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 3,
-                  child: SizedBox(
-                    height: 100,
-                    child: Card(
-                      margin: EdgeInsets.all(10),
-                      elevation: 8,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromRGBO(255, 152, 0, 1),
-                          boxShadow: [
-                            BoxShadow(color: Colors.white, spreadRadius: 3),
-                          ],
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Center(
-                            child: Text(
-                          '30s',
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.bold),
-                        )),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 4,
-                  child: SizedBox(
-                    height: 100,
-                    child: Card(
-                      margin: EdgeInsets.all(10),
-                      elevation: 8,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(color: Colors.white, spreadRadius: 3),
-                          ],
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Center(
-                            child: Text('10+5',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold))),
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 3,
-                  child: SizedBox(
-                    height: 100,
-                    child: Card(
-                      margin: EdgeInsets.all(10),
-                      elevation: 8,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color.fromRGBO(3, 169, 244, 1),
-                          boxShadow: [
-                            BoxShadow(color: Colors.white, spreadRadius: 3),
-                          ],
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Center(
-                            child: Text('10/25',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold))),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            TimerQuestionScoreRow(),
           ],
         ),
       ),
---
+++ lib/widgets/timer_question_score.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class TimerQuestionScoreRow extends StatelessWidget {
+  const TimerQuestionScoreRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      //crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 3,
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              margin: EdgeInsets.all(10),
+              elevation: 8,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromRGBO(255, 152, 0, 1),
+                  boxShadow: [
+                    BoxShadow(color: Colors.white, spreadRadius: 3),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                child: Center(
+                    child: Text(
+                  '30s',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                )),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              margin: EdgeInsets.all(10),
+              elevation: 8,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(color: Colors.white, spreadRadius: 3),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                child: Center(
+                    child: Text('10+5',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              margin: EdgeInsets.all(10),
+              elevation: 8,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color.fromRGBO(3, 169, 244, 1),
+                  boxShadow: [
+                    BoxShadow(color: Colors.white, spreadRadius: 3),
+                  ],
+                ),
+                padding: EdgeInsets.all(10),
+                child: Center(
+                    child: Text('10/25',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
