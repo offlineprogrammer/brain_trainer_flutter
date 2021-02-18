49d41d5d42468290ac760f900298311ee8fb9386
offlineProgrammer <mmalaka@gmail.com>
2021-02-17 20:42:23 -0800

    UI

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -7,10 +7,69 @@ class GameScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Center(
-              child: Text('This is the Game screen'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+              //crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: SizedBox(
+                    height: 100,
+                    child: Card(
+                      margin: EdgeInsets.all(10),
+                      elevation: 8,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text(
+                          '30s',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: SizedBox(
+                    height: 100,
+                    child: Card(
+                      margin: EdgeInsets.all(10),
+                      elevation: 8,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text('10+5',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold))),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: SizedBox(
+                    height: 100,
+                    child: Card(
+                      margin: EdgeInsets.all(10),
+                      elevation: 8,
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Center(
+                            child: Text('10/25',
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold))),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
