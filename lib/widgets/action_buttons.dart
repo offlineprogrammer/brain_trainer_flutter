35bd8e2e0a243961fb4391093ce129b159c3ddce
offlineProgrammer <mmalaka@gmail.com>
2021-02-18 20:39:32 -0800

    Image onTap

--- lib/widgets/action_buttons.dart
+++ lib/widgets/action_buttons.dart
@@ -1,28 +1,47 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class ActionButtons extends StatelessWidget {
+class ActionButtons extends StatefulWidget {
   const ActionButtons({
     Key key,
   }) : super(key: key);
 
   @override
+  _ActionButtonsState createState() => _ActionButtonsState();
+}
+
+class _ActionButtonsState extends State<ActionButtons> {
+  var _imageName = 'assets/images/play.png';
+  Future<Void> _playTheGame(BuildContext context) async {
+    setState(() {
+      _imageName = 'assets/images/playagain.png';
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 100,
-      child: Card(
-        margin: EdgeInsets.all(10),
-        elevation: 8,
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(color: Colors.white, spreadRadius: 3),
-            ],
-          ),
-          padding: EdgeInsets.all(10),
-          child: Image.asset(
-            'assets/images/play.png',
-            fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () {
+          _playTheGame(context);
+        },
+        child: Card(
+          margin: EdgeInsets.all(10),
+          elevation: 8,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(color: Colors.white, spreadRadius: 3),
+              ],
+            ),
+            padding: EdgeInsets.all(10),
+            child: Image.asset(
+              _imageName,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
