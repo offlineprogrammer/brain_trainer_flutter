26f3fd083813e28252a56b429e69276e3c62eef7
offlineProgrammer <mmalaka@gmail.com>
2021-02-23 21:43:16 -0800

    Alert

--- lib/widgets/game_confetti.dart
+++ lib/widgets/game_confetti.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 class GameConfetti extends StatefulWidget {
   @override
@@ -25,27 +26,83 @@ class _GameConfettiState extends State<GameConfetti> {
     super.dispose();
   }
 
+  _showAlert(context) {
+    Future.delayed(Duration.zero, () async {
+      var alertStyle = AlertStyle(
+        animationType: AnimationType.fromTop,
+        isCloseButton: false,
+        isOverlayTapDismiss: false,
+        descStyle: TextStyle(fontWeight: FontWeight.bold),
+        descTextAlign: TextAlign.start,
+        animationDuration: Duration(milliseconds: 400),
+        overlayColor: Colors.transparent,
+        alertBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(0.0),
+          side: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+        titleStyle: TextStyle(
+          color: Colors.red,
+        ),
+        alertAlignment: Alignment.center,
+      );
+
+      Alert(
+        style: alertStyle,
+        context: context,
+        type: AlertType.success,
+        title: "Well done",
+        desc: "You did it",
+        buttons: [
+          DialogButton(
+            child: Text(
+              "COOL",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: () => Navigator.pop(context),
+            width: 120,
+          )
+        ],
+      ).show();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topCenter,
       child: ConfettiWidget(
-        confettiController: _controllerCenter,
-        blastDirectionality: BlastDirectionality
-            .explosive, // don't specify a direction, blast randomly
-        shouldLoop: false,
-        gravity: 0.5,
-        emissionFrequency: 0.05,
-        numberOfParticles:
-            20, // start again as soon as the animation is finished
-        colors: const [
-          Colors.green,
-          Colors.blue,
-          Colors.pink,
-          Colors.orange,
-          Colors.purple
-        ], // manually specify the colors to be used
-      ),
+          confettiController: _controllerCenter,
+          blastDirectionality: BlastDirectionality
+              .explosive, // don't specify a direction, blast randomly
+          shouldLoop: false,
+          gravity: 0.5,
+          emissionFrequency: 0.05,
+          numberOfParticles:
+              20, // start again as soon as the animation is finished
+          colors: const [
+            Colors.green,
+            Colors.blue,
+            Colors.pink,
+            Colors.orange,
+            Colors.purple
+          ], // manually specify the colors to be used
+          child: _showAlert(context)
+
+          /*         AlertDialog(
+          title: Text('Well Done'),
+          content: Text('Great'),
+          actions: [
+            FlatButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        ), */
+          ),
     );
   }
 }
