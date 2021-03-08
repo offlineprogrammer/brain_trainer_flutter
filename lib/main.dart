99a9940d3fdd474e8f64f12bde91344f26415b24
offlineProgrammer <mmalaka@gmail.com>
2021-03-07 21:09:33 -0800

    v1.0

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'BrainTrainer',
         theme: ThemeData(
             primarySwatch: colorCustom,
